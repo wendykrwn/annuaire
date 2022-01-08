d1bf62695c219acc0f8cbed1a97ad6cd62b6cf53
wendy <wendykrwn@gmail.com>
2022-01-08 17:18:05 +0100

    initialisation

---
+++ src/Controller/AnnuaireController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnnuaireController extends AbstractController
+{
+    /**
+     * @Route("/annuaire", name="annuaire")
+     */
+    public function index(): Response
+    {
+        return $this->render('annuaire/index.html.twig', [
+            'controller_name' => 'AnnuaireController',
+        ]);
+    }
+}
