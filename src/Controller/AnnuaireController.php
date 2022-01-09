4d4644832ba06c470092c6da65eb8f83b2e0688c
wendy <wendykrwn@gmail.com>
2022-01-09 06:34:20 +0100

    ajout creation et modification d'un post

--- src/Controller/AnnuaireController.php
+++ src/Controller/AnnuaireController.php
@@ -3,6 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,10 +16,8 @@ class AnnuaireController extends AbstractController
     /**
      * @Route("/posts", name="posts")
      */
-    public function index(): Response
+    public function index(PostRepository $repo): Response
     {
-        $repo = $this->getDoctrine()->getRepository(Post::class);
-
         $posts = $repo->findAll();
 
         return $this->render('annuaire/index.html.twig', [
@@ -31,4 +33,38 @@ class AnnuaireController extends AbstractController
     public function home() {
         return $this->render('annuaire/home.html.twig');
     }
+
+    /**
+     * @Route("/post/new", name="post_create")
+     * @Route("post/{id}/edit", name="post_edit")
+     */
+    public function form(Post $post = null,Request $request, ObjectManager $manager) {
+
+        if(!$post){
+            $post = new Post();
+        }
+
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            if(!$post->getId()){
+                $post->setCreatedAt(new \DateTime());
+            }
+
+            $manager->persist($post);
+            $manager->flush();
+
+            return $this->redirectToRoute('posts');
+        }
+
+        dump($post);
+
+        return $this->render('annuaire/create.html.twig', [
+            'formPost' => $form->createView(),
+            'editMode' => $post->getId() !== null
+        ]);
+    }
+
 }
