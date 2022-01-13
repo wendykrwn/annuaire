4052662a6933c57c198420b77ad77e7c12455c4d
Flayelle Camille <camille@decamille.lan>
2022-01-14 00:23:38 +0100

    profil.user

--- migrations/Version20220113230748.php
+++ migrations/Version20220113230748.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220113195102 extends AbstractMigration
+final class Version20220113230748 extends AbstractMigration
 {
     public function getDescription(): string
     {
